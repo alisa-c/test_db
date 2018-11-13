ac09ef1e53bd271bb58e3273c7f3502b81b85c22
alisa-c <alisachuvilskaya@gmail.com>
2018-11-13 18:04:44 +0000

    Added function which takes node input and outputs the correspoding information

---
+++ test_script.js
@@ -0,0 +1,39 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const myArgs = process.argv.slice(2);
+const userInput = myArgs[0];
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE $1::text LIKE first_name", myArgs, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+
+  function output(result) {
+    console.log(`Found ${result.length} person(s) by the name ${userInput}:`);
+    result.forEach(function(item, i) {
+      let number = 1;
+      console.log(`- ${number + i} ${item.first_name} ${item.last_name} born ${item.birthdate}`);
+    });
+  }
+  output(result.rows);
+
+    client.end();
+  });
+});
+console.log("Searching ...");
+
